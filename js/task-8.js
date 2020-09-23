63de5f8708d67e70247ea0fc40210d6e99fc2aac
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2020-09-23 12:27:08 +0300

    добавляет решение 8-ой задачи

--- js/task-8.js
+++ js/task-8.js
@@ -1,32 +1,42 @@
 const inputNumberEl = document.querySelector("#controls > input");
-console.log(inputNumberEl.value);
-const createBtnEL = document.querySelector(
-  "#controls > button[data-action=render]"
-);
+
+const createBtnEL = document.querySelector('[data-action="render"]');
 createBtnEL.addEventListener("click", onCreateBtnClick);
 
-function onCreateBtnClick(event) {
-  createBoxes(inputNumberEl.value);
-}
-
-function createBoxes(amount) {
-  const arrayEl = Array.from({ length: inputNumberEl.value });
-  arrayEl.map((box) => {
-    const item = document.createElement("div");
-    item.style.backgroundColor =
-      "#" + (Math.random() + "000000").substring(2, 8);
-    item.style.width = "30px";
-    item.style.height = "30px";
-    return boxContainerEl.append(item);
-  });
-}
-
-console.log(createBtnEL);
-
-const destroyBtnEl = document.querySelector(
-  "#controls > button[data-action=destroy]"
-);
-console.log(destroyBtnEl);
+const destroyBtnEl = document.querySelector('[data-action="destroy"]');
+destroyBtnEl.addEventListener("click", onDestroyBtnClick);
 
 const boxContainerEl = document.querySelector("#boxes");
-console.log(boxContainerEl);
+
+function MakeRandomColor() {
+  const randomColor = (min, max) =>
+    Math.round(Math.random() * (max - min) + min);
+
+  const red = randomColor(0, 255);
+  const green = randomColor(0, 255);
+  const blue = randomColor(0, 255);
+
+  return `rgb(${red},${green},${blue})`;
+}
+
+function onCreateBtnClick() {
+  boxContainerEl.innerHTML = "";
+
+  const items = [];
+
+  for (let i = 1; i <= inputNumberEl.value; i += 1) {
+    const divItem = document.createElement("div");
+    console.log(items);
+    divItem.style.backgroundColor = MakeRandomColor();
+    divItem.style.width = i * 10 + 20 + "px";
+    divItem.style.height = i * 10 + 20 + "px";
+
+    items.push(divItem);
+  }
+
+  boxContainerEl.append(...items);
+}
+
+function onDestroyBtnClick() {
+  boxContainerEl.innerHTML = "";
+}
